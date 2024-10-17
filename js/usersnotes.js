c832c9b09271e533618cabff52a28a1bf0f67d2d
Linty <lintydev@gmail.com>
2024-10-17 16:07:55 +0200

    add icon with tooltip notes on each users

--- js/usersnotes.js
+++ js/usersnotes.js
@@ -4,4 +4,39 @@ $(function() {
       'usersnotes_textarea',
       'usernotes',
     );
-  });
+    
+    const target = document.querySelector('.user-container-wrapper');
+    const observer = new MutationObserver(function(mutations) {
+      current_users.forEach((u, i) => {
+        if (u.usernotes) {
+          setIconNote(i, u.usernotes);
+        }
+      })
+    });
+    const config = { childList: true }
+    observer.observe(target, config);
+
+    $('.update-user-button').on('mouseup', function () {
+      const value = $('#usersnotes_textarea').val();
+      if (value.length) {
+        setIconNote(last_user_index, value);
+      } else {
+        removeIconNote(last_user_index);
+      }
+    });
+});
+
+function setIconNote(n, notes) {
+  const iconSelector = `.user-container[key="${n}"] .user-container-username .user-notes-icon`;
+  if (!$(iconSelector).length) {
+    $(`.user-container[key="${n}"] .user-container-username`)
+      .append(`<i class="user-notes-icon icon-info-circled-1 tiptip" title="${notes}"> </i>`);
+  } else {
+    $(iconSelector).attr('title', notes);
+  }
+  $('.user-notes-icon').tipTip();
+}
+
+function removeIconNote(n) {
+  $(`.user-container[key="${n}"] .user-container-username .user-notes-icon`).remove();
+}
